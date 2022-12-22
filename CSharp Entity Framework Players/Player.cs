9ceb427c52c3053bbe2e213eb739c2fcc4fdce5d
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-22 14:14:00 +0100

    Milstone 0 : creati le proprietà e le data annotation

---
+++ CSharp Entity Framework Players/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Entity_Framework_Players
+{
+    internal class Player
+    {
+    }
+}
