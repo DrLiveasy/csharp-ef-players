1834daeb44a25f51df807f80d3a327a9d58917f6
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-22 14:25:33 +0100

    creata la classe CalcioContext per creare la tabella

---
+++ CSharp Entity Framework Players/CalcioContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Entity_Framework_Players
+{
+    public class CalcioContext : DbContext
+    {
+        public DbSet<Player> players { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=CalcioDB;Integrated Security=True");
+        }
+    }
+}
--- CSharp Entity Framework Players/Player.cs
+++ CSharp Entity Framework Players/Player.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharp_Entity_Framework_Players
 {
-    internal class Player
+    [Table("Players")]
+    public class Player
     {
+        #region Proprietà
+
+        [Key]
+        public int PlayerId { get; set; }
+
+        [Required]
+        [Column("nome")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("cognome")]
+        public string Surname { get; set; }
+
+        [Required]
+        [Column("punteggio")]
+        public double Punteggio { get; set; }
+
+        [Required]
+        [Column("partite_giocate")]
+        public int partiteGiocate { get; set; }
+
+        [Required]
+        [Column("partite_vinte")]
+        public int partiteVinte { get; set; }
+
+        #endregion
+
+
     }
 }
