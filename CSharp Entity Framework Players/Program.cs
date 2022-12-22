d8f30c3ae6dd0d9734d5011051de5aa0353bf1d0
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-22 16:03:24 +0100

    MILESTONE 1 : RISOLTO IL BUG ED FINITA

--- CSharp Entity Framework Players/Program.cs
+++ CSharp Entity Framework Players/Program.cs
@@ -4,7 +4,7 @@ using CSharp_Entity_Framework_Players;
 Console.WriteLine("Hello, World!");
 
 //Milestone 1:
-/*
+
 using(CalcioContext db= new CalcioContext())
 {
     Random Casuale = new Random();
@@ -12,13 +12,22 @@ using(CalcioContext db= new CalcioContext())
     int PartiteGiocateGen = Casuale.Next(1, 101);
     int partiteVinteGen = Casuale.Next(0, PartiteGiocateGen + 1);
 
+    //create
     Player giocatore = new Player() { Name = "pinco", Surname = "pallo", Punteggio = PunteggioGen, partiteGiocate = PartiteGiocateGen, partiteVinte = partiteVinteGen};
-
+    db.Add(giocatore);
+    db.SaveChanges();
     Console.WriteLine(giocatore.ToString());
 
+    //update
     giocatore.Name = "marco";
     giocatore.Surname = "pinco";
+    db.SaveChanges();
+    Console.WriteLine("update: "+giocatore.ToString());
+
+    // Delete
+    db.Remove(giocatore);
+    db.SaveChanges();
+    Console.WriteLine("il gio giocatore è stato cancellato");
 
 
-    //cancellazione del giocatore??
-}*/
+}
