b8370526c85f356d1925c6ca672c26a65f6a2550
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-22 15:01:03 +0100

    MILESTONE 1 : Creazione di un nuovo giocatore seconde le regole dell'esercizio

--- CSharp Entity Framework Players/Program.cs
+++ CSharp Entity Framework Players/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharp_Entity_Framework_Players;
+
 Console.WriteLine("Hello, World!");
+
+//Milestone 1:
+using(CalcioContext db= new CalcioContext())
+{
+    Random Casuale = new Random();
+    double PunteggioGen = Casuale.NextDouble() * 9 + 1;
+    int PartiteGiocateGen = Casuale.Next(1, 101);
+    int partiteVinteGen = Casuale.Next(0, PartiteGiocateGen + 1);
+
+    Player giocatore = new Player() { Name = "pinco", Surname = "pallo", Punteggio = PunteggioGen, partiteGiocate = PartiteGiocateGen, partiteVinte = partiteVinteGen};
+
+
+}
